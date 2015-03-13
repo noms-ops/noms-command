ac74ebb0e7a7733d1e5b3b847e7ee49c8aa8a17c
Jeremy Brinkley <jbrinkley@evernote.com>
2015-03-13 02:29:33 -0700

    Dressed up fixtures

--- fixture/dnc.rb
+++ fixture/dnc.rb
@@ -8,34 +8,31 @@ class DNC < Sinatra::Application
     set :port, 8787
     set :root, File.expand_path("#{File.dirname(__FILE__)}")
     enable :static
-    set :public_folder, Proc.new { File.join(root, 'files') }
 
-    File.open(File.join(root, 'dnc.pid'), 'w') {|f| f.puts Process.pid }
+    File.open(File.join(settings.root, 'dnc.pid'), 'w') {|f| f.puts Process.pid }
 
     def load_data
-        JSON.load(File.open(File.join(settings.root, 'files', 'data.json')))
+        JSON.load(File.open(File.join(settings.root, 'public', 'files', 'data.json')))
     end
 
-    def write_data
-        File.open(File.join(settings.root, 'files', 'data.json'), 'w') { |fh| fh << data.to_json }
+    def write_data(data)
+        File.open(File.join(settings.root, 'public', 'files', 'data.json'), 'w') { |fh| fh << data.to_json }
     end
 
     get '/dnc' do
         data = load_data
-        status 200
-        content_type 'application/json'
-        body JSON.pretty_generate(data)
+        [ 200, { 'Content-type' => 'application/json'},
+            JSON.pretty_generate(data) ]
     end
 
     get '/dnc/:id' do
         data = load_data
         object = data.find { |e| e['id'] == params[:id].to_i }
         if object
-            status 200
-            content_type 'application/json'
-            body JSON.pretty_generate(object)
+            [ 200, { 'Content-type' => 'application/json' },
+                JSON.pretty_generate(object) ]
         else
-            status 404
+            404
         end
     end
 
@@ -45,10 +42,12 @@ class DNC < Sinatra::Application
 
         data = load_data
         # How unsafe is this?
-        data << new_object.merge({ 'id' => data.map { |e| e['id'] }.max + 1 })
-        status 201
-        content_type 'application/json'
-        body JSON.pretty_generate(new_object)
+        new_object['id'] = data.map { |e| e['id'] }.max + 1
+        data << new_object
+        write_data data
+
+        [ 201, { 'Content-type' => 'application/json' },
+            JSON.pretty_generate(new_object) ]
     end
 
     put '/dnc/:id' do
@@ -58,25 +57,26 @@ class DNC < Sinatra::Application
         data = load_data
         new_data = data.reject { |e| e['id'] == params[:id].to_i }
         if new_data.size == data.size
-            status 404
+            404
         else
             new_object['id'] = params[:id].to_i
             new_data << new_object
             write_data new_data
-            status 200
-            content_type 'application/json'
-            body JSON.pretty_generate(new_object)
+
+            [ 200, { 'Content-type' => 'application/json' },
+                JSON.pretty_generate(new_object) ]
         end
     end
 
     delete '/dnc/:id' do
-        data = JSON.load
+        data = load_data
         new_data = data.reject { |e| e['id'] == params[:id].to_i }
+
         if new_data.size == data.size
-            status 404
+            404
         else
-            status 204
             write_data new_data
+            204
         end
     end
 
