2d62de850d606d5f0cec785a7e20619a5ff00050
Jeremy Brinkley <jbrinkley@evernote.com>
2015-03-13 01:51:05 -0700

    Example application shaping up

--- Rakefile
+++ Rakefile
@@ -15,6 +15,18 @@ task :testcmd do
     end
 end
 
+# Start the DNC application web server on port 8787
+task :start do
+    FileUtils.rm_r 'test' if File.directory? 'test'
+    system 'cp -R fixture test'
+    system("sh -c '#{RbConfig.ruby} test/dnc.rb >test/dnc.out 2>&1 &'")
+end
+
+task :stop do
+    Process.kill 'TERM', File.read('test/dnc.pid').to_i
+    FileUtils.rm 'test/dnc.pid'
+end
+
 task :clean do
     rm_rf ['pkg', 'test']
 end
--- fixture/dnc.rb
+++ fixture/dnc.rb
@@ -1,30 +1,41 @@
-require 'sinatra'
+require 'sinatra/base'
+require 'json'
 
 # Implement Do Not Call List Example REST application
 # and static file server
 class DNC < Sinatra::Application
-    File.open('dnc.pid', 'w') {|f| f.puts Process.pid }
 
+    set :port, 8787
     set :root, File.expand_path("#{File.dirname(__FILE__)}")
     enable :static
-    set :public_folder, 'dnc'
+    set :public_folder, Proc.new { File.join(root, 'files') }
+
+    File.open(File.join(root, 'dnc.pid'), 'w') {|f| f.puts Process.pid }
+
+    def load_data
+        JSON.load(File.open(File.join(settings.root, 'files', 'data.json')))
+    end
+
+    def write_data
+        File.open(File.join(settings.root, 'files', 'data.json'), 'w') { |fh| fh << data.to_json }
+    end
 
     get '/dnc' do
-        data = JSON.load(File.open('dnc/data.json'))
+        data = load_data
         status 200
         content_type 'application/json'
-        body data
+        body JSON.pretty_generate(data)
     end
 
     get '/dnc/:id' do
-        data = JSON.load(File.open('dnc/data.json'))
-        content_type 'application/json'
-        object = data.find { |e| e['id'] == params[:id] }
+        data = load_data
+        object = data.find { |e| e['id'] == params[:id].to_i }
         if object
-            status 404
-        else
             status 200
-            body object
+            content_type 'application/json'
+            body JSON.pretty_generate(object)
+        else
+            status 404
         end
     end
 
@@ -32,10 +43,9 @@ class DNC < Sinatra::Application
         request.body.rewind
         new_object = JSON.parse request.body.read
 
-        data = JSON.load(File.open('dnc/data.json'))
+        data = load_data
         # How unsafe is this?
         data << new_object.merge({ 'id' => data.map { |e| e['id'] }.max + 1 })
-        File.open('dnc/data.json', 'w') { |fh| fh << data.to_json }
         status 201
         content_type 'application/json'
         body JSON.pretty_generate(new_object)
@@ -45,14 +55,14 @@ class DNC < Sinatra::Application
         request.body.rewind
         new_object = JSON.parse request.body.read
 
-        data = JSON.load(File.open('dnc/data.json'))
-        new_data = data.reject { |e| e['id'] == params['id'] }
+        data = load_data
+        new_data = data.reject { |e| e['id'] == params[:id].to_i }
         if new_data.size == data.size
             status 404
         else
-            new_object['id'] = params['id']
-            data << new_object
-            File.open('dnc/data.json', 'w') { |fh| fh << data.to_json }
+            new_object['id'] = params[:id].to_i
+            new_data << new_object
+            write_data new_data
             status 200
             content_type 'application/json'
             body JSON.pretty_generate(new_object)
@@ -61,12 +71,15 @@ class DNC < Sinatra::Application
 
     delete '/dnc/:id' do
         data = JSON.load
-        new_data = data.reject { |e| e['id'] == params['id'] }
+        new_data = data.reject { |e| e['id'] == params[:id].to_i }
         if new_data.size == data.size
             status 404
         else
             status 204
+            write_data new_data
         end
     end
+
+    run! if app_file = $0
 
 end
